67c1dee06d50ae7c10112e04aee1a73df4f09dfd
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-08-28 21:08:47 -0400

    added styles and icons

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,28 @@
-import {Component} from 'react';
-
+import { Component } from "react";
+import './card-list.styles.css';
 class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
 
-    
-    render() {
-    
-        const {monsters} = this.props;
+    return (
+      <div className = "card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className="card-container" key={id}>
+              <img
+                alt={`monster ${monster.name}`}
+                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+              />
 
-        return(
-            <div>
-                {monsters.map(monster => (
-                    <h1 key = {monster.id}>{monster.name}</h1>
-                ))}
+              <h2>{name}</h2>
+              <p>{email}</p>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default CardList;
